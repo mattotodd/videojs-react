5fda384af2b99e374db9ccb901a77d984ac2e504
Matt Sullivan <mattotodd@gmail.com>
2015-11-14 10:40:12 -0500

    fix compilation

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var require = require || {};
 var gulp = require('gulp');
+var babel = require('gulp-babel');
 var webpack = require('webpack-stream');
  
 var webpack_module = {
@@ -18,15 +19,12 @@ var webpack_module = {
 	}
 }
 gulp.task('scripts', function()  {
-    gulp.src('./src/video.jsx')
-	  .pipe(webpack({
-	    watch: false,
-	    module: webpack_module,
-	    output: {
-        	filename: 'video.js',
-        }
-	  }))
-	  .pipe(gulp.dest('lib/'));
+
+	gulp.src('src/video.jsx')
+        .pipe(babel({
+            presets: ['es2015', 'react']
+        }))
+        .pipe(gulp.dest('lib'));
 
 	gulp.src('./src/example/example.jsx')
 	  .pipe(webpack({
